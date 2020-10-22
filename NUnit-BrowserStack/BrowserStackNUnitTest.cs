83513bdeba9e4787cdb77f3ff5163ad3bbe83324
rajnishk952 <rajnish@browserstack.com>
2020-10-22 18:33:12 +0530

    Fixing local issues

--- NUnit-BrowserStack/BrowserStackNUnitTest.cs
+++ NUnit-BrowserStack/BrowserStackNUnitTest.cs
@@ -5,7 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
-
+using System.Diagnostics;
+using System.Net;
 
 namespace BrowserStack
 {
@@ -15,7 +16,7 @@ namespace BrowserStack
     protected IWebDriver driver;
     protected string profile;
     protected string environment;
-    //private Local browserStackLocal;
+    private Local browserStackLocal;
 
     public BrowserStackNUnitTest(string profile, string environment)
     {
@@ -56,30 +57,63 @@ namespace BrowserStack
       capability.SetCapability("browserstack.user", username);
       capability.SetCapability("browserstack.key", accesskey);
       Object local_cap = capability.GetCapability("browserstack.local");
-      if (local_cap != null && local_cap.ToString().Equals("true"))
-       {
-        //browserStackLocal = new Local();
-            List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
-                    new KeyValuePair<string, string>("key", accesskey)
-            };
-            // BrowserStackLocal.exe file won't work on Mac.
-            //Language Bindings attempts to start the 'exe' file.
-            //For Mac, please start BrowserStackLocal Mac binary separately on command line.
+            Console.WriteLine("Befor checking local");
+            if (local_cap != null && local_cap.ToString().Equals("true"))
+            {
+                //capability.SetCapability("browserstack.localIdentifier", "DummyTest123");
+                //capability.SetCapability("binarypath", "/Users/rajnish/Downloads/BrowserStackLocal");
 
-            //browserStackLocal.start(bsLocalArgs);
-       }
+                
+                browserStackLocal = new Local();
+                Console.WriteLine("Inside local");
 
-      driver = new RemoteWebDriver(new Uri("http://"+ ConfigurationManager.AppSettings.Get("server") +"/wd/hub/"), capability);
-    }
+                //Console.WriteLine(browserStackLocal.isRunning().ToString());
+                List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("key", accesskey),
+                new KeyValuePair<string, string>("binarypath", "/Users/rajnish/Downloads/BrowserStackLocal")
+                };
+
+                if (!browserStackLocal.isRunning())
+                {
+                    Console.WriteLine(browserStackLocal.isRunning());
+                    browserStackLocal.start(bsLocalArgs);
+                    Console.WriteLine(browserStackLocal.isRunning());
+                }
+             
+            }
+
+            driver = new RemoteWebDriver(new Uri("http://"+ ConfigurationManager.AppSettings.Get("server") +"/wd/hub/"), capability);
+
+            //REST API call
+
+            string reqString = "{\"status\":\"passed\", \"reason\":\"Test Passed\"}";
+            string sessionId = ((RemoteWebDriver)driver).Capabilities.GetCapability("webdriver.remote.sessionid").ToString();
+            byte[] requestData = System.Text.Encoding.UTF8.GetBytes(reqString);
+            Uri myUri = new Uri(string.Format($"https://www.browserstack.com/automate/sessions/{sessionId}.json"));
+            System.Net.WebRequest myWebRequest = System.Net.HttpWebRequest.Create(myUri);
+            System.Net.HttpWebRequest myHttpWebRequest = (System.Net.HttpWebRequest)myWebRequest;
+            myWebRequest.ContentType = "application/json";
+            myWebRequest.Method = "PUT";
+            myWebRequest.ContentLength = requestData.Length;
+            using (System.IO.Stream st = myWebRequest.GetRequestStream()) st.Write(requestData, 0, requestData.Length);
+
+            System.Net.NetworkCredential myNetworkCredential = new NetworkCredential("rajnishkumar21", "shzMytdTq5WXMzKqkT81");
+            CredentialCache myCredentialCache = new CredentialCache();
+            myCredentialCache.Add(myUri, "Basic", myNetworkCredential);
+            myHttpWebRequest.PreAuthenticate = true;
+            myHttpWebRequest.Credentials = myCredentialCache;
+
+            myWebRequest.GetResponse().Close();
+        }
 
     [TearDown]
     public void Cleanup()
     {
       driver.Quit();
-      //if (browserStackLocal != null)
-      //{
-      //  browserStackLocal.stop();
-      //}
-    }
+            if (browserStackLocal != null)
+            {
+                browserStackLocal.stop();
+            }
+        }
   }
 }
---
+++ NUnit-BrowserStack/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SingleTest
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
